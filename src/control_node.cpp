b6b6a2071d6e3b6131b35ee667967a725fdf37e4
serenity3747 <3747juho@gmail.com>
2023-04-04 04:43:44 +0900

    20230403

--- src/control_node.cpp
+++ src/control_node.cpp
@@ -10,12 +10,14 @@
 #include <tf/tf.h>
 #include <std_msgs/Float64.h>
 
-// #include "offb/cmdsrv.h""
+// #include "offb/cmdsrv.h"
 
 #define height 4
 #define xmargin 1.7
 #define ymargin 1.5
 #define positionerror 0.05
+#define position_diff_x 0
+#define position_diff_y 2
 
 geometry_msgs::Point leftlower,rightupper;
 geometry_msgs::PoseStamped cur_local0,cur_local1;
@@ -32,31 +34,91 @@ geometry_msgs::PoseStamped uav1_target;
 // }
 
 
-class values{
+class pubclass{
     public:
+        pubclass(){
+            targetpub0 = nh_.advertise<geometry_msgs::PoseStamped>
+            ("uav0/target_position",1);
+        
+            targetpub1 = nh_.advertise<geometry_msgs::PoseStamped>
+            ("uav1/target_position",1);
+            
+            while(cur_local0.pose.position.x-uav0_target.pose.position.x<positionerror&&cur_local0.pose.position.y-uav0_target.pose.position.y<positionerror&&cur_local1.pose.position.x-uav1_target.pose.position.x<positionerror&&cur_local1.pose.position.y-uav1_target.pose.position.y<positionerror){
+                targetpub0.publish(uav0_target);
+                targetpub1.publish(uav1_target);
+    }
     
+        }
+        void uav0(float x, float y){
+            uav0_target.pose.position.x = x;
+            uav0_target.pose.position.y = y;
+            uav0_target.pose.position.z = height;
+        }
+
+        void uav1(float x, float y){
+            uav1_target.pose.position.x = x;
+            uav1_target.pose.position.y = y-position_diff_y;
+            uav1_target.pose.position.z = height;
+        }
+
     
+        void runOp(){
+            if(cur_local0.pose.position.x-leftlower.y<rightupper.y-cur_local0.pose.position.y&&cur_local1.pose.position.y-leftlower.y<rightupper.y-cur_local1.pose.position.y){
+                uav0(cur_local0.pose.position.x,rightupper.y-ymargin);
+                uav1(cur_local1.pose.position.y,rightupper.y-ymargin);
+                targetpub0.publish(uav0_target);
+                targetpub1.publish(uav1_target);
+                
+                if(cur_local0.pose.position.x>leftlower.x&&rightupper.x<cur_local1.pose.position.x){
+                    uav0(cur_local0.pose.position.x-xmargin*2,cur_local0.pose.position.y);
+                    uav1(cur_local1.pose.position.x+xmargin*2,cur_local1.pose.position.y);
+
+                }
+            }
+            else if(cur_local0.pose.position.y-leftlower.y>rightupper.y-cur_local0.pose.position.y&&cur_local1.pose.position.y-leftlower.y>rightupper.y-cur_local1.pose.position.y){
+                uav0(cur_local0.pose.position.x,leftlower.y+ymargin);
+                uav1(cur_local1.pose.position.x,leftlower.y+ymargin);
+                targetpub0.publish(uav0_target);
+                targetpub1.publish(uav1_target);
+                
+                if(cur_local0.pose.position.x>leftlower.x&&rightupper.x<cur_local1.pose.position.x){
+                    uav0(cur_local0.pose.position.x-xmargin*2,cur_local0.pose.position.y);
+                    uav1(cur_local1.pose.position.x+xmargin*2,cur_local1.pose.position.y);
+
+                }
+            }
+        }
+
+    private:
+        ros::NodeHandle nh_;
+        ros::Publisher targetpub0;
+        ros::Publisher targetpub1;
 };
 
 
-void cur_positionCB0(geometry_msgs::PoseStamped::ConstPtr& msg){
+
+
+
+
+
+void cur_positionCB0(const geometry_msgs::PoseStamped::ConstPtr& msg){
     cur_local0=*msg;
 }
 
-void cur_positionCB1(geometry_msgs::PoseStamped::ConstPtr& msg){
+void cur_positionCB1(const geometry_msgs::PoseStamped::ConstPtr& msg){
     cur_local1=*msg;
 }
-void uav0(float x, float y){
-    uav0_target.pose.position.x = x;
-    uav0_target.pose.position.y = y;
-    uav0_target.pose.position.z = height;
-}
+// void uav0(float x, float y){
+//     uav0_target.pose.position.x = x;
+//     uav0_target.pose.position.y = y;
+//     uav0_target.pose.position.z = height;
+// }
 
-void uav1(float x, float y){
-    uav1_target.pose.position.x = x;
-    uav1_target.pose.position.y = y-2;
-    uav1_target.pose.position.z = height;
-}
+// void uav1(float x, float y){
+//     uav1_target.pose.position.x = x;
+//     uav1_target.pose.position.y = y-2;
+//     uav1_target.pose.position.z = height;
+// }
 
 int main(int argc, char **argv)
 {
@@ -65,17 +127,20 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
     ros::NodeHandle nh_private("~");
 
+    pubclass pubclass;
+
     nh_private.getParam("leftlower_x",leftlower.x);
     nh_private.getParam("leftlower_y",leftlower.y);
     nh_private.getParam("rightupper_x",rightupper.x);
     nh_private.getParam("rightupper_y",rightupper.y);
 
 
-    ros::Publisher targetpub0 = nh.advertise<geometry_msgs::PoseStamped>
-        ("uav0/target_position",1);
+
+    // ros::Publisher targetpub0 = nh.advertise<geometry_msgs::PoseStamped>
+    //     ("uav0/target_position",1);
     
-    ros::Publisher targetpub1 = nh.advertise<geometry_msgs::PoseStamped>
-        ("uav1/target_position",1);
+    // ros::Publisher targetpub1 = nh.advertise<geometry_msgs::PoseStamped>
+    //     ("uav1/target_position",1);
     // ros::Subscriber cmd = nh.subscribe
     ros::Subscriber cur_position0 = nh.subscribe<geometry_msgs::PoseStamped> 
         ("uav0/mavros/local_position/pose", 10, cur_positionCB0);
@@ -87,24 +152,24 @@ int main(int argc, char **argv)
     //     ("cmdsrv",ctrl_func); 
 
     //시작점으로가기
-    uav0((leftlower.x+rightupper.x)/2-xmargin,leftlower.y+ymargin);
-    uav1((leftlower.x+rightupper.x)/2+xmargin,leftlower.y+ymargin);
+    pubclass.uav0((leftlower.x+rightupper.x)/2-xmargin,leftlower.y+ymargin);
+    pubclass.uav1((leftlower.x+rightupper.x)/2+xmargin,leftlower.y+ymargin);
 
 
-    while(cur_local0.pose.position.x-uav0_target.pose.position.x<positionerror&&cur_local0.pose.position.y-uav0_target.pose.position.y<positionerror&&cur_local1.pose.position.x-uav1_target.pose.position.x<positionerror&&cur_local1.pose.position.y-uav1_target.pose.position.y<positionerror){
-        targetpub0.publish(uav0_target);
-        targetpub1.publish(uav1_target);
-    }
+    // while(cur_local0.pose.position.x-uav0_target.pose.position.x<positionerror&&cur_local0.pose.position.y-uav0_target.pose.position.y<positionerror&&cur_local1.pose.position.x-uav1_target.pose.position.x<positionerror&&cur_local1.pose.position.y-uav1_target.pose.position.y<positionerror){
+    //     pubclass.targetpub0.publish(uav0_target);
+    //     pubclass.targetpub1.publish(uav1_target);
+    // }
     
     //사이클 시작
     ros::Rate rate(2.0);
 
     while(ros::ok()){
-        runOp();
+        pubclass.runOp();
         ros::spinOnce();
         rate.sleep();
-        if(cur_local0.x<leftlower.x&&cur_local1.x>rigthupper.x){
-            runOp();
+        if(cur_local0.pose.position.x<leftlower.x&&cur_local1.pose.position.x>rightupper.x){
+            pubclass.runOp();
             break;
         }
 
@@ -116,29 +181,36 @@ int main(int argc, char **argv)
 }
 
 
-void runOp(){
-    if(cur_local0.y-leftlower.y<rightupper.y-cur_local0.y&&cur_local1.y-leftlower.y<rightupper.y-cur_local1.y){
-        uav0(cur_local0.x,rightupper.y-ymargin);
-        uav1(cur_local1.x,rightupper.y-ymargin);
-        targetpub0.publish(uav0_target);
-        targetpub1.publish(uav1_target);
-        
-        if(cur_local0.x>leftlower.x&&rightupper.x<cur_local1.x){
-            uav0(cur_local0.x-xmargin*2,cur_local0.y);
-            uav1(cur_local1.x+xmargin*2,cur_local1.y);
 
-        }
-    }
-    else if(cur_local0.y-leftlower.y>rightupper.y-cur_local0.y&&cur_local1.y-leftlower.y>rightupper.y-cur_local1.y){
-        uav0(cur_local0.x,leftlower.y+ymargin);
-        uav1(cur_local1.x,leftlower.y+ymargin);
-        targetpub0.publish(uav0_target);
-        targetpub1.publish(uav1_target);
-        
-        if(cur_local0.x>leftlower.x&&rightupper.x<cur_local1.x){
-            uav0(cur_local0.x-xmargin*2,cur_local0.y);
-            uav1(cur_local1.x+xmargin*2,cur_local1.y);
 
-        }
-    }
-}
+
+
+
+
+
+// void runOp(){
+//     if(cur_local0.pose.position.x-leftlower.y<rightupper.y-cur_local0.pose.position.y&&cur_local1.pose.position.y-leftlower.y<rightupper.y-cur_local1.pose.position.y){
+//         uav0(cur_local0.pose.position.x,rightupper.y-ymargin);
+//         uav1(cur_local1.pose.position.y,rightupper.y-ymargin);
+//         targetpub0.publish(uav0_target);
+//         targetpub1.publish(uav1_target);
+        
+//         if(cur_local0.pose.position.x>leftlower.x&&rightupper.x<cur_local1.pose.position.x){
+//             uav0(cur_local0.pose.position.x-xmargin*2,cur_local0.pose.position.y);
+//             uav1(cur_local1.pose.position.x+xmargin*2,cur_local1.pose.position.y);
+
+//         }
+//     }
+//     else if(cur_local0.pose.position.y-leftlower.y>rightupper.y-cur_local0.pose.position.y&&cur_local1.pose.position.y-leftlower.y>rightupper.y-cur_local1.pose.position.y){
+//         uav0(cur_local0.pose.position.x,leftlower.y+ymargin);
+//         uav1(cur_local1.pose.position.x,leftlower.y+ymargin);
+//         targetpub0.publish(uav0_target);
+//         targetpub1.publish(uav1_target);
+        
+//         if(cur_local0.pose.position.x>leftlower.x&&rightupper.x<cur_local1.pose.position.x){
+//             uav0(cur_local0.pose.position.x-xmargin*2,cur_local0.pose.position.y);
+//             uav1(cur_local1.pose.position.x+xmargin*2,cur_local1.pose.position.y);
+
+//         }
+//     }
+// }
